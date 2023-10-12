63606c8db66a1cad1be90b2a1f2293a367c6d74d
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-12 14:50:58 +0900

    Feat: counter service complete

--- src/app/users-service.ts
+++ src/app/users-service.ts
@@ -1,4 +1,5 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import {Injectable } from "@angular/core";
+import { CounterService } from "./counter-service";
 
 @Injectable()
 
@@ -7,15 +8,19 @@ export class UsersService{
     activeUsers = ['Max', 'Anna'];
     inactiveUsers = ['Chris', 'Manu'];
 
+    constructor(private counterService: CounterService){}
+
 
     setToInactive(id: number) {
         this.inactiveUsers.push(this.activeUsers[id]);
         this.activeUsers.splice(id, 1);
+        this.counterService.updateActiveToInactive();
       }
 
       setToActive(id: number) {
         this.activeUsers.push(this.inactiveUsers[id]);
         this.inactiveUsers.splice(id, 1);
+        this.counterService.updateInactiveToActive();
       }
 
 }
