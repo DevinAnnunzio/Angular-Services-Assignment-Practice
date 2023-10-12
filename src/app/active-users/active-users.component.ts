87c1235b1029441b50d83cf62998029da441c898
DevinAnnunzio <annunzio.da@gmail.com>
2023-10-12 14:59:11 +0900

    Docs: updating naming of service files to what I think might be correct naming convention

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { UsersService } from '../users-service';
+import { UsersService } from '../users.service';
 
 @Component({
   selector: 'app-active-users',
--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,5 +1,5 @@
 import {Injectable } from "@angular/core";
-import { CounterService } from "./counter-service";
+import { CounterService } from "./counter.service";
 
 @Injectable()
 
